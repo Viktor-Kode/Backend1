3efd4ac2d6ec6098787fadd398bea3777f67ecb6
viktor <victorand804@gmail.com>
2026-01-04 11:31:06 +0100

    'finishday3'

--- src/server.js
+++ src/server.js
@@ -34,24 +34,66 @@
 //  })
 
 
-import express from 'express'
+// import express from 'express'
 
+// const app = express();
+// app.use(express.json());
+
+// app.get("/shop/:category", (req, res) => {
+//   const category = req.params.category; // The main "shelf"
+//   const color = req.query.color;       // The optional "filter"
+//   if (color) {
+//     res.send(`You are looking at ${color} ${category}.`);
+//   } else {
+//     res.send(`You are looking at all ${category}.`);
+//   }
+// });
+
+// const PORT = 5001;
+// app.listen(PORT, ()=>{
+//     console.log(`server running at port: ${PORT}`);
+    
+// })
+
+
+
+
+// import express from 'express'
+// const app = express();
+
+
+// app.use(express.json());
+
+// // app.get('/books/:category', (req,res)=>{
+// //   const cat= req.params.category
+// //   const page = req.query.page
+
+
+// //   res.send(`<h1>you are reading a book about ${cat}, you are at page ${page}.</h1>`)
+// // })
+// const PORT = 5001;
+// app.listen(PORT,()=>{
+//   console.log(`testing port at${PORT}`);
+  
+// })
+
+import express from 'express'
 const app = express();
+
 app.use(express.json());
 
-app.get("/shop/:category", (req, res) => {
-  const category = req.params.category; // The main "shelf"
-  const color = req.query.color;       // The optional "filter"
+app.post('/join-gym', (req, res) => {
+  const name = req.body.name;
+  const goal = req.body.goal;
 
-  if (color) {
-    res.send(`You are looking at ${color} ${category}.`);
-  } else {
-    res.send(`You are looking at all ${category}.`);
-  }
+  // Use res.json to send the data back to Thunder Client
+  res.json({
+    message: `Welcome viktor`,
+    plan: `We will help you marathon`
+  });
 });
 
 const PORT = 5001;
-app.listen(PORT, ()=>{
-    console.log(`server running at port: ${PORT}`);
-    
+app.listen(PORT,()=>{
+  console.log(`testing port at${PORT}`)
 })
